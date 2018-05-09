9c70932c092454fe64a074f96fb8aaa006403d2f
Mihaela MJ <mihaelamj@me.com>
2018-05-10 00:51:59 +0200

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,6 +4,6 @@ import Vapor
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
-public func adRoutes(_ router: Router) throws {
+public func routes(_ router: Router) throws {
   try addSQLiteRoutes(router)
 }
